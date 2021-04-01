a7c0cd91d5746d32ea0da3fa1c254bacf340702a
rohan sondharva <rohansondharva@gmail.com>
2021-04-01 12:12:11 +0530

    Added Emoji

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,33 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐵", "🐵",
+                "🦁", "🦁",
+                "😺", "😸",
+                "🐷", "🐷",
+                "🐶", "🐶",
+                "🐸", "🐸",
+                "🦊", "🦊",
+                "🐼", "🐼",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
         }
     }
 }
